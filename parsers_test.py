76ab0b91d9047ef10cd21868d56147fc4c4ec553
DanyloSih <geratosfen@gmail.com>
2022-10-13 18:08:21 +0300

    MNE POHUUI

--- parsers_test.py
+++ parsers_test.py
@@ -1,33 +1,7 @@
-import time
-from threading import Thread
-from selenium import webdriver
 import parsers
+import driver_creation
 
-
-def create_driver_threaded(thread_id: int, result_container: list):
-    options = webdriver.FirefoxOptions()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    result_container[thread_id] = webdriver.Firefox(options=options)
-
-
-def create_drivers(drivers_count: int):
-    start_time = time.time()
-    results = [None] * drivers_count
-    threads = [None] * drivers_count
-    for i in range(drivers_count):
-        threads[i] = Thread(target=create_driver_threaded, args=(i, results))
-        threads[i].start()
-
-    for i in range(drivers_count):
-        threads[i].join()
-
-    print('Drivers count:', drivers_count)
-    print('Drivers start time:', time.time() - start_time)
-    return results
-
-
-drivers = create_drivers(2)
+drivers = driver_creation.create_drivers(2)
 whitebit_parser = parsers.WhitebitParser(drivers[0])
 binance_parser = parsers.BinanceParser(drivers[1])
 
